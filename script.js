0bde5e9ec60d53ad299128b1b76056cd3bcb5b30
darod1n <50169301+darod1n@users.noreply.github.com>
2021-01-17 11:13:01 +0300

    upd

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-(function init(){
-    const cards = Array.from(document.querySelectorAll('.card'));
-    cards.forEach((card) => {card.addEventListener('click', clickEvent); });
-}());
-
-function clickEvent(){
+function fillCards(cards, emoji) {
+    cards.forEach((card, index) => { card.textContent = emoji[index]; });
+  }
+  
+  function coupleArr(arr) {
+    return [].concat(arr, arr);
+  }
+  
+  function shuffleArr(arr) {
+    return arr.sort(() => Math.random() - 0.5);
+  }
+  
+  function clickEvent() {
     const opened = this.classList.contains('open');
-    if (!opened){
-        this.classList.remove('close');
-        this.classList.add('open');
+    if (!opened) {
+      this.classList.remove('close');
+      this.classList.add('open');
     } else {
-        this.classList.remove('open');
-        this.classList.add('close');
+      this.classList.remove('open');
+      this.classList.add('close');
     }
-}
+  }
+  
+  (function init() {
+    const emoji = '🐞 🦀 🐟 🐊 🐓 🦃'.split(' ');
+    const shuffleEmoji = shuffleArr(coupleArr(emoji));
+    const cards = document.querySelectorAll('.card');
+    fillCards(cards, shuffleEmoji);
+    cards.forEach((card) => { card.addEventListener('click', clickEvent); });
+  }());
